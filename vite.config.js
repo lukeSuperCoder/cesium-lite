f5d62654cea1643b2cbd5a19dd93d0a06ebbf640
luke <luke@elane.com.cn>
2025-07-02 16:32:18 +0800

    更新 Vite 配置，添加环境变量加载功能，并优化构建选项以支持多个入口文件，保持服务器端口设置不变。

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,28 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import cesium from "vite-plugin-cesium";
-export default defineConfig({
-  plugins: [cesium()],
-  server: {
-    port: 8020,
+import { resolve } from "path";
+
+export default defineConfig(({ mode }) => {
+  // 读取环境变量
+  const env = loadEnv(mode, process.cwd());
+  return {
+    base: env.VITE_BASE || '/',
+    plugins: [cesium()],
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          basicMap: resolve(__dirname, 'examples/basicMap.html'),
+          camera: resolve(__dirname, 'examples/camera.html'),
+          draw: resolve(__dirname, 'examples/draw.html'),
+          entity: resolve(__dirname, 'examples/entity.html'),
+          marker: resolve(__dirname, 'examples/marker.html'),
+          tileLayer: resolve(__dirname, 'examples/tileLayer.html'),
+        }
+      }
+    },
+    server: {
+      port: 8020,
+    }
   }
 });
